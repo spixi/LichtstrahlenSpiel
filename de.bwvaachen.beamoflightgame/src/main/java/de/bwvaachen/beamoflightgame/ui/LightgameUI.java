a2d9a450a23277657d506cc0fd135918753ba9d1
Bastian <b.winzen@gmx.de>
2013-11-19 22:28:29 +0100

    GUI Team schaut euch mal an wie der JFilechooser funktioniert habe schonmal exemplarisch einen in Andys ActionListener gesetzt

--- de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/ui/LightgameUI.java
+++ de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/ui/LightgameUI.java
@@ -1,35 +1,30 @@
 package de.bwvaachen.beamoflightgame.ui;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Dimension;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JSeparator;
-import javax.swing.JProgressBar;
-
-import org.easymock.EasyMock;
-
-import de.bwvaachen.beamoflightgame.controller.ILightController;
-import de.bwvaachen.beamoflightgame.model.IBeamsOfLightPuzzleBoard;
-import de.bwvaachen.beamoflightgame.model.INumberTile;
-import de.bwvaachen.beamoflightgame.model.ITile;
-import de.bwvaachen.beamoflightgame.model.LightTileState;
-
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Iterator;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JSeparator;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileFilter;
+
+import org.easymock.EasyMock;
+
+import de.bwvaachen.beamoflightgame.controller.ILightController;
+import de.bwvaachen.beamoflightgame.model.IBeamsOfLightPuzzleBoard;
 
 public class LightgameUI extends JFrame {
 
@@ -38,6 +33,7 @@ public class LightgameUI extends JFrame {
 	 */
 	private static final long serialVersionUID = -3961364231837270604L;
 	private JPanel contentPane;
+	private File lastSaveFile=null;
 	private ILightController controller=new ILightController() {
 		
 		@Override
@@ -132,6 +128,17 @@ public class LightgameUI extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//Dialog zum ermitteln des Dateipfades einbinden
+				
+				JFileChooser fileChooser=new JFileChooser();
+				FileFilter filter = new ExtensionFileFilter("Beam of light puzzle saves", new String[] { "BOL" });
+				fileChooser.setFileFilter(filter);
+				if(fileChooser.showOpenDialog(LightgameUI.this)!=JFileChooser.CANCEL_OPTION){
+					File selectedFile = fileChooser.getSelectedFile();
+					if(selectedFile!=null&& selectedFile.exists()){
+							//Öffnen
+					}
+					
+				}
 				try {
 					//im Design ist das hier nicht void sondern gibt ein Puzzle zurück, das sollten wir dann aufbauen
 					controller.load(new File(""));
@@ -155,5 +162,48 @@ public class LightgameUI extends JFrame {
 		});
 	}
 	
-	
+	class ExtensionFileFilter extends FileFilter {
+		  String description;
+
+		  String extensions[];
+
+		  public ExtensionFileFilter(String description, String extension) {
+		    this(description, new String[] { extension });
+		  }
+
+		  public ExtensionFileFilter(String description, String extensions[]) {
+		    if (description == null) {
+		      this.description = extensions[0];
+		    } else {
+		      this.description = description;
+		    }
+		    this.extensions = (String[]) extensions.clone();
+		    toLower(this.extensions);
+		  }
+
+		  private void toLower(String array[]) {
+		    for (int i = 0, n = array.length; i < n; i++) {
+		      array[i] = array[i].toLowerCase();
+		    }
+		  }
+
+		  public String getDescription() {
+		    return description;
+		  }
+
+		  public boolean accept(File file) {
+		    if (file.isDirectory()) {
+		      return true;
+		    } else {
+		      String path = file.getAbsolutePath().toLowerCase();
+		      for (int i = 0, n = extensions.length; i < n; i++) {
+		        String extension = extensions[i];
+		        if ((path.endsWith(extension) && (path.charAt(path.length() - extension.length() - 1)) == '.')) {
+		          return true;
+		        }
+		      }
+		    }
+		    return false;
+		  }
+		}
 }
