f1898f7ace1f91d94d7d9f9e71366240f9252a01
fruehholz_chr <chris_fruehholz@live.de>
2013-11-29 13:09:19 +0100

    TurnUndoManager

---
+++ de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/controller/TurnUndoManager.java
@@ -0,0 +1,11 @@
+package de.bwvaachen.beamoflightgame.controller;
+
+import javax.swing.undo.UndoManager;
+import javax.swing.undo.UndoableEdit;
+
+public class TurnUndoManager extends UndoManager
+{
+	
+	//UndoableEdit marker = new UndoableEdit();
+	
+}
