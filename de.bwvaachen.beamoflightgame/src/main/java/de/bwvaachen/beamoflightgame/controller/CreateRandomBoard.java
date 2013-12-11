a5ad519a6b31b5c91e394c4151d576354ece49ed
cfruehholz <chris_fruehholz@live.de>
2013-12-11 16:58:25 +0100

    - Algorithmus zur Berechnung des NumberTileCounts erstellt - Versucht NumberTiles im Board hinzuzufügen

--- de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/controller/CreateRandomBoard.java
+++ de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/controller/CreateRandomBoard.java
@@ -8,6 +8,10 @@ public class CreateRandomBoard
 	{
 		//1,5*math.sqrt(x,y)+-20%
 		//TODO Tiles Holen, Anzahl NumberTiles berechnen, NumberTiles Random setzen, LightTiles auffüllen, Zahlen vergeben
+		double dblNumberTileCount;
+		dblNumberTileCount = (1.5 * Math.sqrt(oBoard.getHeight()*oBoard.getWidth())/100*(Math.random()*20));
+		oBoard.putTile(oBoard.getTileAt((int)(oBoard.getHeight()*Math.random()), (int)(oBoard.getWidth()*Math.random())));		
+		
 		return oBoard;
 	}
 	
