6f7229a6d900547b1991907991d02f1fa8f62170
fruehholz_chr <chris_fruehholz@live.de>
2013-12-13 09:28:12 +0100

    kleine Änderung

--- de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/controller/CreateRandomBoard.java
+++ de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/controller/CreateRandomBoard.java
@@ -1,5 +1,6 @@
 package de.bwvaachen.beamoflightgame.controller;
 
+import de.bwvaachen.beamoflightgame.model.NumberTile;
 import de.bwvaachen.beamoflightgame.model.impl.BeamsOfLightPuzzleBoard;
 
 public class CreateRandomBoard 
@@ -10,6 +11,12 @@ public class CreateRandomBoard
 		//TODO Tiles Holen, Anzahl NumberTiles berechnen, NumberTiles Random setzen, LightTiles auffüllen, Zahlen vergeben
 		double dblNumberTileCount;
 		dblNumberTileCount = (1.5 * Math.sqrt(oBoard.getHeight()*oBoard.getWidth())/100*(Math.random()*20));
+		NumberTile[] oNumTiles;
+		for (int i = 0; i <= dblNumberTileCount; i++)
+			{
+				oNumTiles[i] = new NumberTile(oBoard, oBoard.getHeight()*Math.random(), (int)(oBoard.getWidth()*Math.random()), i);
+			}
+		
 		oBoard.putTile(oBoard.getTileAt((int)(oBoard.getHeight()*Math.random()), (int)(oBoard.getWidth()*Math.random())));		
 		
 		return oBoard;
