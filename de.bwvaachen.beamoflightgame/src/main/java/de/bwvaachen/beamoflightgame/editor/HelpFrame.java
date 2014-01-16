779ec22b73d9c8090151193fc47bfc91af5f6685
Chris <drslater@gmx.net>
2014-01-16 22:05:09 +0100

    Gelernt wie man neue Dateien ins github repository läd :D

---
+++ de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/editor/HelpFrame.java
@@ -0,0 +1,44 @@
+package de.bwvaachen.beamoflightgame.editor;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+@SuppressWarnings("serial")
+public class HelpFrame extends JFrame {
+	
+	private FileReader 		reader;
+	private BufferedReader 	br;
+	private JTextArea		text;
+	private Dimension		screenSize;
+	
+	public HelpFrame(String path) throws IOException{
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(400,600) ;
+		
+		text = new JTextArea();
+		reader = new FileReader(path);
+		br = new BufferedReader(reader);
+        text.read(br,null);
+        br.close();
+        text.requestFocus();
+        
+        setLayout(new BorderLayout());
+		getContentPane().add(new JScrollPane(text),BorderLayout.CENTER);
+		
+		setLocation((screenSize.width - getSize().width)/2,(screenSize.height - getSize().height)/2);
+		setResizable(false);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("BeamsOfLightGame - Help");
+		pack();
+		setVisible(true);
+	}
+
+}
