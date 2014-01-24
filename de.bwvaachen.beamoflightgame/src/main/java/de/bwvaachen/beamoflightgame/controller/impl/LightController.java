15c7ab3404ec21a1dd218508707ba3fc2e25e066
spixi <marius.spix@web.de>
2014-01-24 13:12:04 +0100

    .

--- de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/controller/impl/LightController.java
+++ de.bwvaachen.beamoflightgame/src/main/java/de/bwvaachen/beamoflightgame/controller/impl/LightController.java
@@ -1,7 +1,7 @@
 package de.bwvaachen.beamoflightgame.controller.impl;
 
 /*
-Copyright (C) 2013 - 2014 by Georg Braun, Christian Frühholz, Marius Spix, Christopher Müller and Bastian Winzen Part of the Beam Of Lights Puzzle Project
+Copyright (C) 2013 - 2014 by Andreas Pauls, Georg Braun, Christian Frühholz, Marius Spix, Christopher Müller and Bastian Winzen Part of the Beam Of Lights Puzzle Project
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
@@ -61,9 +61,10 @@ public class LightController implements ILightController {
 		ZipPersister persister = new ZipPersister(new SimpleASCIICodec());
 		try {
 			
-			Pair<IBeamsOfLightPuzzleBoard,List<Turn>> pair = persister.load(f);
+			Pair<IBeamsOfLightPuzzleBoard[],List<Turn>> pair = persister.load(f);
 			
-			this.setBoard(pair.left);
+			this.setBoard(pair.left[0]);
+			this.solutionBoard = pair.left[1];
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -85,7 +86,7 @@ public class LightController implements ILightController {
 	public void saveGame(File f) throws IOException {
 				
 		ZipPersister persister = new ZipPersister(new SimpleASCIICodec());
-		persister.save(f, puzzleBoard, turnManager.getTurns()); 
+		persister.save(f, puzzleBoard, turnManager.getTurns(),solutionBoard); 
 				
 	} // public void saveGame(File f)
 	
@@ -145,11 +146,14 @@ public class LightController implements ILightController {
 		}
 		
 		puzzleBoard = _board ;
-		solutionBoard = null;
+		if(solutionBoard == null)
+		{		
+			solve();
+		}
 		turnManager = new TurnUndoManager();
 		
 		puzzleBoard.addUndoableEditListener(turnManager);
-		solve();
+
 		notifyBoardChangeListeners();
 	}
 
